ab32e7a4bc0d0fc7d8938297054087ac4cb2751e
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-20 13:05:20 -0300

    Creating my first app in Android with Kotlin

--- WhatsMyName/app/src/main/java/com/example/bruno/whatsmyname/MainActivity.kt
+++ WhatsMyName/app/src/main/java/com/example/bruno/whatsmyname/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.bruno.whatsmyname
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var enteredName = enter_name_edt_id.text
+
+        show_name_button_id.setOnClickListener {
+            result_text_id.text = enteredName
+        }
     }
 }
