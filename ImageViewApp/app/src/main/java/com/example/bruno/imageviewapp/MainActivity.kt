921a80bd97b7b3cc4f4779817a559cc73c9b72c6
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-22 17:30:55 -0300

    Learning how about to play with ImageView filters.

--- ImageViewApp/app/src/main/java/com/example/bruno/imageviewapp/MainActivity.kt
+++ ImageViewApp/app/src/main/java/com/example/bruno/imageviewapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.bruno.imageviewapp
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cat_img_id.setColorFilter(Color.BLUE)
+        dog_img_id.setColorFilter(Color.CYAN)
+
+        cat_img_id.setOnClickListener {
+            cat_img_id.clearColorFilter()
+//            cat_img_id.setColorFilter(Color.GREEN, PorterDuff.Mode.DARKEN)
+            cat_img_id.setBackgroundColor(Color.MAGENTA)
+        }
     }
 }
