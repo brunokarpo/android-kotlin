7ea445589395dcfce5cfb0b0b6e63e358a7f53a0
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-22 15:44:06 -0300

    Improving app "What's your weight" with new planets

--- YourWeightOn/app/src/main/java/com/example/bruno/yourweighton/MainActivity.kt
+++ YourWeightOn/app/src/main/java/com/example/bruno/yourweighton/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.bruno.yourweighton
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.CheckBox
 import kotlinx.android.synthetic.main.activity_main.*
 
 private fun Double.format(digits: Int) = String.format("%.${digits}f", this)
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private val marsGravity: Float = 0.38f
+    private val venusGravity: Float = 0.91f
+    private val jupterGravity: Float = 2.34f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,21 @@ class MainActivity : AppCompatActivity() {
             var result = calculateWeight(weight.toString().toDouble())
 
             result_text_view_id.text = "You weight is ${result.format(2)} on Mars"
+        }
+
+        mars_checkbox_id.setOnClickListener(this)
+        venus_checkbox_id.setOnClickListener(this)
+        jupter_checkbox_id.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        view as CheckBox
+        var isChecked: Boolean = view.isChecked
+
+        when(view.id) {
+            R.id.mars_checkbox_id -> if(isChecked) Log.d("Setup Mars", "Mars")
+            R.id.venus_checkbox_id -> if(isChecked) Log.d("Setup Venus", "Venus")
+            R.id.jupter_checkbox_id -> if(isChecked) Log.d("Setup Jupter", "Jupter")
         }
     }
 
