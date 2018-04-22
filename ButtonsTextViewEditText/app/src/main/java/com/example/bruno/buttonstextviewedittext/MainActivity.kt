9d84742380de5b4a47dbde065620a8dc542e8107
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-22 10:25:00 -0300

    Implementing Kotlin part of activity

--- ButtonsTextViewEditText/app/src/main/java/com/example/bruno/buttonstextviewedittext/MainActivity.kt
+++ ButtonsTextViewEditText/app/src/main/java/com/example/bruno/buttonstextviewedittext/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.bruno.buttonstextviewedittext
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var name = enter_name_edt_id.text
+        show_name_button_id.setOnClickListener {
+            var message = "Enter name"
+            if (!name.isEmpty()) {
+                message = "Welcome $name"
+            }
+            result_txt_id.text = message
+        }
     }
 }
