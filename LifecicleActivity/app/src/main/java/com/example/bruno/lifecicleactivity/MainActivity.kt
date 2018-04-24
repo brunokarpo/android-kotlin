06c101b7c7e623731d5fbb53d3433888aaa7c804
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-23 21:49:58 -0300

    Overrinding functions of activity lifecicle.

--- LifecicleActivity/app/src/main/java/com/example/bruno/lifecicleactivity/MainActivity.kt
+++ LifecicleActivity/app/src/main/java/com/example/bruno/lifecicleactivity/MainActivity.kt
@@ -2,11 +2,44 @@ package com.example.bruno.lifecicleactivity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "OnCreate Started", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart Started", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume Started", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause Started", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop Started", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart Started", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy Started", Toast.LENGTH_SHORT).show()
     }
 }
