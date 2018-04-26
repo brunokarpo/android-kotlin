966aa715d6db422ccec699f7736eb29c9522941f
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-26 12:49:01 -0300

    Learning how to create a simple list view.

--- SimpleArray/app/src/main/java/com/example/bruno/simplearray/MainActivity.kt
+++ SimpleArray/app/src/main/java/com/example/bruno/simplearray/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.bruno.simplearray
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ListViews = requires adapters and datasource
+
+        // datasource: datas
+        var namesArray: Array<String> = arrayOf("Jenny", "Paul", "Santos", "Lee", "Riuji", "Jenny", "Paul", "Santos", "Lee", "Riuji"
+                                                ,"Jenny", "Paul", "Santos", "Lee", "Riuji", "Jenny", "Paul", "Santos", "Lee", "Riuji")
+
+
+        // adapter: adapt the datasource
+        var namesAdapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, namesArray)
+
+        list_view_id.adapter = namesAdapter
+
+        list_view_id.setOnItemClickListener { parent, view, position, id ->
+            var itemName: String = list_view_id.getItemAtPosition(position).toString()
+            Toast.makeText(this, "Name: $itemName", Toast.LENGTH_LONG).show()
+        }
     }
 }
