162d000e6a338ab04f1b5dea0aefcfba9d4ad661
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-27 13:34:44 -0300

    Implementing creating of row and the list View.

--- ReciclerListViewsApp/app/src/main/java/com/example/bruno/reciclerlistviewsapp/MainActivity.kt
+++ ReciclerListViewsApp/app/src/main/java/com/example/bruno/reciclerlistviewsapp/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.bruno.reciclerlistviewsapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.example.bruno.reciclerlistviewsapp.data.PersonListAdapter
+import com.example.bruno.reciclerlistviewsapp.model.Person
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var adapter: PersonListAdapter? = null
+    private var personList: ArrayList<Person>? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        personList = arrayListOf()
+        layoutManager = LinearLayoutManager(this)
+        adapter = PersonListAdapter(personList!!, this)
+
+
+        recycler_view_id.layoutManager = layoutManager
+        recycler_view_id.adapter = adapter
+
+        for(i in 0..16) {
+            val person = Person("James $i", 20 + i, R.mipmap.ic_launcher)
+            personList!!.add(person)
+        }
+
+        adapter!!.notifyDataSetChanged()
+
     }
 }
--- ReciclerListViewsApp/app/src/main/java/com/example/bruno/reciclerlistviewsapp/data/PersonListAdapter.kt
+++ ReciclerListViewsApp/app/src/main/java/com/example/bruno/reciclerlistviewsapp/data/PersonListAdapter.kt
@@ -2,26 +2,42 @@ package com.example.bruno.reciclerlistviewsapp.data
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.bruno.reciclerlistviewsapp.R
 import com.example.bruno.reciclerlistviewsapp.model.Person
 
 class PersonListAdapter(private val list: ArrayList<Person>,
                         private val context: Context) : RecyclerView.Adapter<PersonListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(context).inflate(R.layout.my_list_row, parent, false)
+
+        return ViewHolder(view, context)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bindItem(list.get(position))
     }
 
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    class ViewHolder(itemView: View, val context: Context): RecyclerView.ViewHolder(itemView) {
+        fun bindItem(person: Person) {
+            var name: TextView = itemView.findViewById(R.id.name_text_id)
+            var age: TextView = itemView.findViewById(R.id.age_text_id)
+            var image: ImageView = itemView.findViewById(R.id.pic_view_id)
+
+            name.text = person.name
+            age.text = person.age.toString()
+            image.setImageDrawable(context.applicationContext.resources.getDrawable(person.idImage!!))
+        }
+    }
 
 }
