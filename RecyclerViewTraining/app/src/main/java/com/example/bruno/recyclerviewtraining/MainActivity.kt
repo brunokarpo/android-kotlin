0e35f09e1f79204f129efaf48621ff1d1df53a35
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-02 18:33:12 -0300

    Creating project to trainning Recycler View

---
+++ RecyclerViewTraining/app/src/main/java/com/example/bruno/recyclerviewtraining/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bruno.recyclerviewtraining
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
