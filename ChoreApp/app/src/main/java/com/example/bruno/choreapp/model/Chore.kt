0c7fd496d0f57e36bebb80e6da8674eb8116e041
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-03 18:17:26 -0300

    Creating model class Chore

---
+++ ChoreApp/app/src/main/java/com/example/bruno/choreapp/model/Chore.kt
@@ -0,0 +1,9 @@
+package com.example.bruno.choreapp.model
+
+class Chore(
+        var choreName: String? = null,
+        var assignedBy: String? = null,
+        var assignedTo: String? = null,
+        var timeAssigned: Long? = null,
+        var id: Integer? = null
+    ) {}
