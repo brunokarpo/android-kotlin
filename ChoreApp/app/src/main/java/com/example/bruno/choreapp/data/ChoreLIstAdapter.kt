e8f023f633a336b3ad2d303d15a7c8437ef333b8
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-03 21:25:44 -0300

    Creating adapter to fill our chore row list

---
+++ ChoreApp/app/src/main/java/com/example/bruno/choreapp/data/ChoreLIstAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.bruno.choreapp.data
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.bruno.choreapp.R
+import com.example.bruno.choreapp.model.Chore
+import java.util.*
+
+class ChoreLIstAdapter(
+        private val list: ArrayList<Chore>,
+        private val context: Context
+    ): RecyclerView.Adapter<ChoreLIstAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view = LayoutInflater
+                .from(context)
+                .inflate(R.layout.chore_list_row, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(list[position])
+    }
+
+    class ViewHolder(itemView: View?): RecyclerView.ViewHolder(itemView) {
+
+        private var choreName = itemView!!.findViewById(R.id.list_chore_name_text_id) as TextView
+        private var assignedBy = itemView!!.findViewById(R.id.list_assigned_by_text_id) as TextView
+        private var assignedTo = itemView!!.findViewById(R.id.list_assigned_to_text_id) as TextView
+        private var assignedDate = itemView!!.findViewById(R.id.list_chore_date_text_id) as TextView
+
+        fun bindItem(chore: Chore) {
+
+            choreName.text = chore.choreName
+            assignedBy.text = chore.assignedBy
+            assignedTo.text = chore.assignedTo
+            assignedDate.text = chore.showHumanDate()
+
+        }
+    }
+}
--- ChoreApp/app/src/main/java/com/example/bruno/choreapp/model/Chore.kt
+++ ChoreApp/app/src/main/java/com/example/bruno/choreapp/model/Chore.kt
@@ -1,9 +1,20 @@
 package com.example.bruno.choreapp.model
 
+import java.text.DateFormat
+import java.util.*
+
 class Chore(
         var choreName: String? = null,
         var assignedBy: String? = null,
         var assignedTo: String? = null,
         var timeAssigned: Long? = null,
         var id: Int? = null
-    ) {}
+    ) {
+
+    fun showHumanDate(): String {
+        var dateFormat: java.text.DateFormat = DateFormat.getDateInstance()
+        var formattedDate: String = dateFormat.format(Date(timeAssigned!!).time)
+
+        return "Created $formattedDate"
+    }
+}
