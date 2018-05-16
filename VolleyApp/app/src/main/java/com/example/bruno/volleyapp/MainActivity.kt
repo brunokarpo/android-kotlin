7ca5125622ad0119aa084eba95139c84800da634
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-16 18:07:31 -0300

    Learning how to catch JSON array from a Volley request.

--- VolleyApp/app/src/main/java/com/example/bruno/volleyapp/MainActivity.kt
+++ VolleyApp/app/src/main/java/com/example/bruno/volleyapp/MainActivity.kt
@@ -7,13 +7,16 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 import org.json.JSONException
 
 class MainActivity : AppCompatActivity() {
     var volleyRequest: RequestQueue? = null
     val stringLink: String = "https://www.magadistudio.com/complete-android-developer-course-source-files/string.html"
+    val jsonLink: String = "https://private-2ce0c-brunokarpo.apiary-mock.com/people"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,33 @@ class MainActivity : AppCompatActivity() {
         volleyRequest = Volley.newRequestQueue(this)
 
         getString(stringLink)
+        getJsonArray(jsonLink)
+    }
+
+    fun getJsonArray(url: String) {
+        val jsonReq = JsonArrayRequest(Request.Method.GET, url,
+                Response.Listener {
+                    response: JSONArray ->
+                    try {
+                        Log.d("Response ========>>", response.toString())
+
+                        for(i in 0 until response.length() - 1) {
+                            val peopleObj = response.getJSONObject(i)
+
+                            var nome = peopleObj.getString("name")
+                            Log.d("Person name: ", nome)
+
+                        }
+                    } catch (e: JSONException) { e.printStackTrace() }
+                },
+                Response.ErrorListener {
+                    error: VolleyError? ->
+                    try {
+                        Log.e("Error", error!!.toString())
+
+                    } catch (ex: JSONException) { ex.printStackTrace() }
+                })
+        volleyRequest!!.add(jsonReq)
     }
 
     fun getString(url: String) {
