98fa144f43e9b14839efa04bab8d4b3bb189c598
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-21 17:27:33 -0300

    Learning about how to retrieve data from Firebase reference

--- IntroToKotlinFirebase/app/src/main/java/com/example/bruno/introtokotlinfirebase/MainActivity.kt
+++ IntroToKotlinFirebase/app/src/main/java/com/example/bruno/introtokotlinfirebase/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.bruno.introtokotlinfirebase
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var firebaseDatabase = FirebaseDatabase.getInstance()
-        var databaseRef = firebaseDatabase.reference
+        var databaseRef = firebaseDatabase.getReference("messages")
 
         databaseRef.setValue("Hello There")
+
+
+        // Read from our DB
+        databaseRef.addValueEventListener(object : ValueEventListener{
+
+            override fun onDataChange(dataSnapshot: DataSnapshot?) {
+                var value = dataSnapshot!!.value
+
+                Log.d("Value====>>>", value.toString())
+            }
+
+            override fun onCancelled(databaseError: DatabaseError?) {
+                Log.e("Error=====>>", databaseError!!.toString())
+            }
+        })
     }
 }
