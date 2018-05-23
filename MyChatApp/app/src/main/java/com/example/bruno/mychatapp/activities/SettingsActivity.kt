95f79e4443d60401576b4d8d9939f5e130791b4f
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-23 19:51:18 -0300

    Implementing code of Settings Activity and creating status activity

--- MyChatApp/app/src/main/java/com/example/bruno/mychatapp/activities/SettingsActivity.kt
+++ MyChatApp/app/src/main/java/com/example/bruno/mychatapp/activities/SettingsActivity.kt
@@ -1,15 +1,56 @@
 package com.example.bruno.mychatapp.activities
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.example.bruno.mychatapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+
+
+    var mDatabaseReference: DatabaseReference? = null
+    var mCurrentUser: FirebaseUser? = null
+    var mStorage: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        mCurrentUser = FirebaseAuth.getInstance().currentUser
+
+        var userUID = mCurrentUser!!.uid
+
+        mDatabaseReference = FirebaseDatabase.getInstance().reference
+                .child("Users")
+                .child(userUID)
+
+        mDatabaseReference!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot?) {
+                var displayName = snapshot!!.child("display_name").value
+                var image = snapshot!!.child("image").value
+                var status = snapshot!!.child("status").value
+                var thumbnail = snapshot!!.child("thumb_image").value
+
+                settings_status_text_id.text = status.toString()
+                settings_display_name.text = displayName.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError?) {
+
+            }
+        })
+
+
+        settings_text_status_button_id.setOnClickListener {
+            var intent = Intent(this, StatusActivity::class.java)
+            intent.putExtra("status", settings_status_text_id.text.toString().trim())
+            startActivity(intent)
+        }
 
     }
 }
---
+++ MyChatApp/app/src/main/java/com/example/bruno/mychatapp/activities/StatusActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bruno.mychatapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StatusActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_status)
+    }
+}
