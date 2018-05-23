43c8d62504ed3ce278438e23bf7c02df45598a20
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-23 10:08:29 -0300

    Implementing verification if user has logged in.

--- MyChatApp/app/src/main/java/com/example/bruno/mychatapp/MainActivity.kt
+++ MyChatApp/app/src/main/java/com/example/bruno/mychatapp/MainActivity.kt
@@ -3,13 +3,33 @@ package com.example.bruno.mychatapp
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var mAuth: FirebaseAuth? = null
+    var user: FirebaseUser? =  null
+    var mAuthListener: FirebaseAuth.AuthStateListener? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            firebaseAuth: FirebaseAuth ->
+            user = firebaseAuth.currentUser
+
+            if (user != null) {
+                startActivity(Intent(this, DashboardActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Not Signed in", Toast.LENGTH_LONG).show()
+            }
+        }
 
         main_create_account_button_id.setOnClickListener{
             startActivity(Intent(this, CreateAccountActivity::class.java))
@@ -17,6 +37,19 @@ class MainActivity : AppCompatActivity() {
 
         main_login_button_id.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mAuth!!.addAuthStateListener(mAuthListener!!)
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (mAuthListener != null) {
+            mAuth!!.removeAuthStateListener(mAuthListener!!)
         }
     }
 }
