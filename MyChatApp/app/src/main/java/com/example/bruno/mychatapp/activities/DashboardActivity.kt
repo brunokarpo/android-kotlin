dbefda94669e89fd2f3aed21e7645d575a0b8b8a
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-23 19:18:29 -0300

    Creating and implementing the menu.

--- MyChatApp/app/src/main/java/com/example/bruno/mychatapp/activities/DashboardActivity.kt
+++ MyChatApp/app/src/main/java/com/example/bruno/mychatapp/activities/DashboardActivity.kt
@@ -1,11 +1,15 @@
 package com.example.bruno.mychatapp.activities
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.example.bruno.mychatapp.R
 import com.example.bruno.mychatapp.adapters.SectionPagerAdapter
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
@@ -28,5 +32,31 @@ class DashboardActivity : AppCompatActivity() {
 
             Toast.makeText(this, username, Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        super.onOptionsItemSelected(item)
+
+        if (item != null) {
+            when (item!!.itemId) {
+                R.id.logout_id -> {
+                    FirebaseAuth.getInstance().signOut()
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
+                R.id.settings_id -> {
+                    startActivity(Intent(this, SettingsActivity::class.java))
+                }
+            }
+        }
+
+        return true
     }
 }
---
+++ MyChatApp/app/src/main/java/com/example/bruno/mychatapp/activities/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.bruno.mychatapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bruno.mychatapp.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        
+    }
+}
