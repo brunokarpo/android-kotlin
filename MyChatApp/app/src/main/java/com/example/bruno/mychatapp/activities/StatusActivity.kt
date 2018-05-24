b38de1f2f59721ecc4f5291b38999a249d177b94
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-24 12:54:31 -0300

    Implemented status update.

--- MyChatApp/app/src/main/java/com/example/bruno/mychatapp/activities/StatusActivity.kt
+++ MyChatApp/app/src/main/java/com/example/bruno/mychatapp/activities/StatusActivity.kt
@@ -1,10 +1,15 @@
 package com.example.bruno.mychatapp.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.bruno.mychatapp.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_status.*
 
 class StatusActivity : AppCompatActivity() {
@@ -27,7 +32,30 @@ class StatusActivity : AppCompatActivity() {
         }
 
         status_update_button_id.setOnClickListener {
-            
+
+            mCurrentUser = FirebaseAuth.getInstance().currentUser
+            var userId = mCurrentUser!!.uid
+
+            mDatabaseReference = FirebaseDatabase.getInstance().reference
+                    .child("Users").child(userId)
+
+            var status = status_update_edit_text_id.text.toString().trim()
+
+            mDatabaseReference!!.child("status")
+                    .setValue(status).addOnCompleteListener {
+                        task: Task<Void> ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Status updated successufully!", Toast.LENGTH_LONG)
+                                    .show()
+
+                            startActivity(Intent(this, SettingsActivity::class.java))
+                            finish()
+                        } else {
+                            Toast.makeText(this, "Status not updated!", Toast.LENGTH_LONG)
+                                    .show()
+                        }
+                    }
+
         }
     }
 }
